b7c542e864237c12f0bdea332026ef8370cbd61c
hkan <hermann.kan.dev@gmail.com>
2022-10-16 17:54:27 -0400

    completed bbsort func

--- sorting-algo/bubble-sort/bubbleSort.js
+++ sorting-algo/bubble-sort/bubbleSort.js
@@ -4,7 +4,14 @@ function bubbleSort(input) {
   let swapping = true;
   while (swapping) {
     swapping = false;
-    for (let i = 0; i < input.length - 1; i++) {}
+    for (let i = 0; i < input.length - 1; i++) {
+      if (input[i] > input[i + 1]) {
+        //log a message for every swap made when we execute bubbleSort()
+        console.log(`Swapping pair ${input[i]}, ${input[i + 1]} in [${input}]`);
+        swap(input, i, i + 1);
+        swapping = true;
+      }
+    }
   }
   return input;
 }
---
+++ sorting-algo/bubble-sort/script.js
@@ -0,0 +1,6 @@
+const bubbleSort = require("./bubbleSort");
+const inputOne = [9, 8, 7, 6, 5, 4, 3, 2, 1];
+
+const inputTwo = [(1, 2, 3, 4, 5, 6, 7, 8, 9)];
+
+bubbleSort(inputOne);
