2b7a592d9caccff7af8563d5c73dbfcd38c430bd
hkan <hermann.kan.dev@gmail.com>
2022-10-18 21:32:21 -0400

    completed graph

---
+++ graph/Graph.js
@@ -0,0 +1,14 @@
+const Edge = require("./Edge.js");
+const Vertex = require("./Vertex.js");
+
+class Graph {
+  constructor() {
+    this.vertices = [];
+  }
+  print() {
+    const vertexList = this.vertices || [];
+    vertexList.forEach((vertex) => vertex.print());
+  }
+}
+
+module.exports = Graph;
