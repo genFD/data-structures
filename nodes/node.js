97979ed047766e98ce93e316714516d1925586a9
hkan <hermann.kan.dev@gmail.com>
2022-10-23 08:33:08 -0400

    updated Node class

--- nodes/node.js
+++ nodes/node.js
@@ -3,15 +3,25 @@ class Node {
     this.data = data;
     this.next = null;
   }
-  setNextNode(node) {
-    if (node instanceof Node || node === null) {
-      this.next = node;
-    } else {
-      throw new Error("Next node must be a member of the Node class.");
-    }
-  }
   getNextNode() {
     return this.next;
+  }
+  getPreviousNode() {
+    return this.previous;
+  }
+  setNextNode(node) {
+    if (node instanceof Node || node == null) {
+      this.next = node;
+    } else {
+      throw new Error("Next node must be instance of node or Null");
+    }
+  }
+  setPreviousNode(node) {
+    if (node instanceof Node || node === null) {
+      this.previous = node;
+    } else {
+      throw new Error("Previous node must be a member of the Node class");
+    }
   }
 }
 
