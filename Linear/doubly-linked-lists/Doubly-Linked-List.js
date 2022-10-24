807b826118c603f312967c55b60bdde7f6f41135
hkan <hermann.kan.dev@gmail.com>
2022-10-24 16:07:39 -0400

    completed removefromhead removefromtail removebydata

--- Linear/doubly-linked-lists/Doubly-Linked-List.js
+++ Linear/doubly-linked-lists/Doubly-Linked-List.js
@@ -39,6 +39,59 @@ class DoublyLinkedList {
       this.head = new Node(data);
     }
   }
+  removeFromTail() {
+    const removedTail = this.tail;
+    if (!removedTail) {
+      return;
+    }
+    this.tail = removedTail.getPreviousNode();
+    if (this.tail) {
+      this.tail.setNextNode(null);
+    }
+    if (removedTail == this.head) {
+      this.removeFromHead();
+    }
+    return removedTail.data;
+  }
+  removeFromHead() {
+    const removedHead = this.head;
+    if (!removedHead) {
+      return;
+    }
+    this.head = removedHead.getNextNode();
+    if (this.head) {
+      this.head.setPreviousNode(null);
+    }
+    if (removedHead == this.tail) {
+      this.removeFromTail();
+    }
+    return removedHead.data;
+  }
+  removeByData(data) {
+    let nodeToRemove;
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      if (currentNode.data === data) {
+        nodeToRemove = currentNode;
+        break;
+      }
+      currentNode = currentNode.getNextNode();
+    }
+    if (!nodeToRemove) {
+      return null;
+    }
+    if (nodeToRemove === this.head) {
+      this.removeHead();
+    } else if (nodeToRemove === this.tail) {
+      this.removeTail();
+    } else {
+      const nextNode = nodeToRemove.getNextNode();
+      const previousNode = nodeToRemove.getPreviousNode();
+      nextNode.setPreviousNode(previousNode);
+      previousNode.setNextNode(nextNode);
+    }
+    return nodeToRemove.data;
+  }
   printList() {
     let currentNode = this.head;
     let output = "";
