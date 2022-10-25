b23a2d028fbb02332dc282e2098cee3f4fee2d37
hkan <hermann.kan.dev@gmail.com>
2022-10-25 19:26:06 -0400

    completed enqueue

--- Linear/queue/Queue.js
+++ Linear/queue/Queue.js
@@ -1,7 +1,21 @@
-class Queue {
-  constructor(){
-  
-  }
+const LinkedList = require("../singly-linked-lists/LinkedList");
 
+class Queue {
+  constructor() {
+    this.queue = new LinkedList();
+    //when queue is first initialized it has 0 nodes
+    this.size = 0;
+  }
+  /* 
+  ----API------
+  enqueue(5)
+  dequeue(3)
+  peek()
+  */
+  enqueue(data) {
+    this.queue.addToTail(data);
+    this.size++;
+    console.log(`Added ${data}! Queue size is now ${this.size}`);
+  }
 }
 module.exports = Queue;
--- Linear/singly-linked-lists/LinkedList.js
+++ Linear/singly-linked-lists/LinkedList.js
@@ -42,3 +42,70 @@ class LinkedList {
   }
 }
 module.exports = LinkedList;
+
+/* 
+// ###-------ADDTOHEAD-------######
+
+-------------------------------------------
+The idea                                 ««
+-------------------------------------------
+
+The idea is to 
+- add a new node before the current list head if the list is not empty i.e list has already a head 
+- otherwise if the list is empty we just want to set the list head to our new Node.
+
+--------------------------------------------
+How to implement this                     ««
+--------------------------------------------
+
+//* need new head in any cases 
+newHead = create new node
+
+//* need keep track of the current list head
+currentHead = list head
+
+//* any cases set list head to our new head
+list head =  newHead
+
+//* if list is not empty
+
+if currentHead is not null then
+   set list head next node to current Head
+
+****pseudo:
+
+1 newHead = create new node
+2 currentHead = list head
+3 list head =  newHead
+4 if currentHead is not null then
+5   set list head next node to current Head
+
+// ###-------ADDTOTAIL-------######
+
+
+-------------------------------------------
+The idea                                 ««
+-------------------------------------------
+
+The idea is to 
+- find the last node in the list
+- add a new node directly after it
+if the list is empty ie list head is null then our new node becomes head and tail.
+
+
+pseudo:
+newTail  = create new node
+currentTail = list head
+if list head is null then
+   list head = newTail
+else
+while node after current node is not null then
+  move forward one node : set currentTail to node directly after it 
+  ---
+set currentTail's next node to new node/new tail
+  
+// ###-------REMOVEFROMTAIL-------######
+
+// ###-------REMOVEBYDATA-------######
+
+*/
